effe21c5f4806437caa8afecc1a3701940a94d8e
Rae Stichter <raestichter@gmail.com>
2021-02-05 23:17:01 -0500

    server.js file interaction with MySQL WorkBench setup

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// ------------------------- SETUP -------------------------
 // Dependencies
 var express = require('express');
 var exphbs  = require('express-handlebars');
@@ -11,7 +12,8 @@ const db_config = require("./config/config");
 // Create an instance of the expresss app.
 var app = express();
 
-// Added so body parser can handle post requests.  If we didn't have this the body would come back as undefined
+// Added so body parser can handle post requests.
+// If we didn't have this the body would come back as undefined
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -27,12 +29,30 @@ var PORT = process.env.PORT || 9090;
 app.engine("handlebars", exphbs({ deafaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// ROUTES
-app.get("/", function (req, res) {
-    res.render ("index");
+
+
+// ------------------------- ROUTES (test) -------------------------
+app.get("/", function(req, res) {
+  let connection = mysql.createConnection(db_config);
+  let promisedParanormal_locations = new Promise((resolve, reject) => {
+    connection.query("SELECT * FROM paranormal_locations;", function(err, data) {
+     console.log(data);
+      if (err) {
+        return res.status(500).end();
+      }
+      connection.end();
+      // output from '\views\index.handlebars'
+      res.render("index");
+    });
+  });
+  promisedParanormal_locations.then((successMessage) => {
+    // successMessage is whatever we passed in the resolve(...) function above.
+    // It doesn't have to be a string, but if it is only a succeed message, it probably will be.
+    res.send(successMessage);
+  }); 
 });
 
-
+// ------------------------- APP.LISTEN -------------------------
 app.listen(PORT, function() {
     // Log (server-side) when our server has started
     console.log("Server listening on http://localhost:" + PORT);
