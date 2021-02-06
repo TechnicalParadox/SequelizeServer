3eab95a890d9e9a13c02d9d382d536416ac38d14
Rae Stichter <raestichter@gmail.com>
2021-02-05 23:23:47 -0500

    test heroku 2

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ app.get("/", function(req, res) {
     // successMessage is whatever we passed in the resolve(...) function above.
     // It doesn't have to be a string, but if it is only a succeed message, it probably will be.
    // res.send(successMessage);
-  }); 
+  //}); 
 });
 
 // ------------------------- APP.LISTEN -------------------------
